2f248c616967fd004f9712c2fee110389038eaca
ptitbob <github@shipstone.org>
2019-03-03 00:06:48 +0100

    Ajout de la lib simple

--- application/src/main/java/org/shipstone/sandbox/springlib/application/Application.java
+++ application/src/main/java/org/shipstone/sandbox/springlib/application/Application.java
@@ -1,9 +1,11 @@
 package org.shipstone.sandbox.springlib.application;
 
+import org.shipstone.sandbox.springlib.library.EnableLibrary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableLibrary
 public class Application {
 
   public static void main(String[] args) {
