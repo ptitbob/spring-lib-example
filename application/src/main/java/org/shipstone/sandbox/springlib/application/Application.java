2bd74d1fe709f4d85f747678854cf022ee1edc16
ptitbob <github@shipstone.org>
2019-03-03 23:17:27 +0100

    Ajout de la lib data avec la déclaration d'un repository, service et controller

--- application/src/main/java/org/shipstone/sandbox/springlib/application/Application.java
+++ application/src/main/java/org/shipstone/sandbox/springlib/application/Application.java
@@ -1,11 +1,13 @@
 package org.shipstone.sandbox.springlib.application;
 
+import org.shipstone.sandbox.springlib.libdata.EnableLibData;
 import org.shipstone.sandbox.springlib.library.EnableLibrary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableLibrary
+@EnableLibData
 public class Application {
 
   public static void main(String[] args) {
